f8c3257dd9b88ebcb1c3b752898f8c6b362fa4a2
Mohammad Adnan <65945728+adnandotdev@users.noreply.github.com>
2025-03-30 23:45:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const checkboxes = document.querySelectorAll(".toggle")
+let selected = []
+checkboxes.forEach((checkBox) => {
+	checkBox.addEventListener("change", function () {
+		// const checked = document.querySelectorAll(".toggle:checked")
+		if(this.checked){
+			selected.push(this)
+		}
+		else{
+			selected = selected.filter( box => box !== this)
+		}
+		if(selected.length>2){
+			let firstChecked = selected.shift(); 
+            firstChecked.checked = false;
+		}
+	})
+})
